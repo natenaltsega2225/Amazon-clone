786d240dc3b4eb116d3023998d85757faeee8a4a
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-09-19 16:53:11 -0500

    Add files to enable API functionality

--- src/Components/Carousel/img/data.js
+++ src/Components/Carousel/img/data.js
@@ -1,10 +1,10 @@
-import img1 from './blanket.jpeg'
-import img2 from './double_tshirts.jpeg'
-import img3 from './pc_holder.jpeg'
-import img4 from './sandal.jpeg'
-import img5 from './sport_drinks.jpeg'
-import img6 from './fan.jpeg'
+import img1 from './prime_video.jpeg'
+import img2 from './water_bottle.jpeg'
+import img3 from './decor.jpeg'
+import img4 from './fashion.jpeg'
+import img5 from './prescription.jpeg'
 
 
 
-export const img = [img1, img2, img3,img4,img5,img6]
+
+export const img = [img1, img2, img3,img4,img5]
---
+++ src/Pages/Landing/Landing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import LayOut from '../../Components/LayOut/LayOut'
+import Carousel from '../../Components/Carousel/CarouselEffect'
+import Category from '../../Components/Category/Category'
+import Product from '../../Components/Product/Product'
+function Landing() {
+  return (
+    <div>
+        <LayOut>
+        <Carousel/>
+        <Category/>
+        <Product/>
+      </LayOut>
+
+
+    </div>
+  )
+}
+
+export default Landing;
