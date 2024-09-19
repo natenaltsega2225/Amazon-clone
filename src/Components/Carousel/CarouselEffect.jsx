089b8c7b2f6fd3dad3c6d157860aabbac9e22a9c
Natnael Tsega <42552538+natenaltsega2225@users.noreply.github.com>
2024-09-19 16:55:46 -0500

    Implemented a carousel component to showcase featured products

--- src/Components/Carousel/CarouselEffect.jsx
+++ src/Components/Carousel/CarouselEffect.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import classes from './Carousel.module.css'; // Import the CSS classes
-import { img } from './img/data';
+import classes from "./Carousel.module.css"; // Import the CSS classes
+import { img } from "./img/data";
 
 function CarouselEffect() {
   return (
-    <div > 
+    <div>
       <Carousel
         autoPlay={true}
         infiniteLoop={true}
@@ -15,9 +15,7 @@ function CarouselEffect() {
       >
         {img.map((imageItemLink) => {
           return <img src={imageItemLink} />;
-        })
-        
-      }
+        })}
       </Carousel>
       <div className={classes.hero__img}></div>
     </div>
